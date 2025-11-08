fd2ae3957d7c45a29c1ad3f796c2014aba723066
Bismutotantalite/Biz | Biotica <reymarkgaming123@gmail.com>
2025-11-08 19:05:44 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,8 @@
-const corpuscles = Vars.mods.locateMod("corpuscles").meta;
+const corpuscles = Vars.mods.locateMod("Corpuscles").meta;
 
 corpuscles.displayName = Core.bundle.get("mod.corpuscles.name");
 corpuscles.author = Core.bundle.get("mod.corpuscles.author");
 corpuscles.description = Core.bundle.get("mod.corpuscles.description");
 
 corpuscles.subtitle = Core.bundle.get("mod.corpuscles.subtitle");
+
