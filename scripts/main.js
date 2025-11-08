e212ba981fde6693701d310456616a9ac4838e50
Kevin Vilyan <146146416+Kev-Vily@users.noreply.github.com>
2025-11-08 14:41:28 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
-const corpuscles = Vars.mods.locateMod("Corpuscles").meta;
+const corpuscles = Vars.mods.locateMod("corpuscles").meta;
 
 corpuscles.displayName = Core.bundle.get("mod.corpuscles.name");
 corpuscles.author = Core.bundle.get("mod.corpuscles.author");
 corpuscles.description = Core.bundle.get("mod.corpuscles.description");
+
 corpuscles.subtitle = Core.bundle.get("mod.corpuscles.subtitle");
