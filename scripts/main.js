cf4b536ca26c580c18c093c38c0b271479edee75
Bismutotantalite/Biz | Corpuscles <reymarkgaming123@gmail.com>
2025-11-14 14:09:28 +0800

    fix fix

--- scripts/main.js
+++ scripts/main.js
@@ -6,4 +6,14 @@ corpuscles.description = Core.bundle.get("mod.corpuscles.description");
 
 corpuscles.subtitle = Core.bundle.get("mod.corpuscles.subtitle");
 
+SaveFileReader.fallback.putAll(
+  "biotic-zlo-distribution-aluminum-duct", "biotic-zlo-distribution-aluminum-channel",
+  "biotic-zlo-distribution-aluminum-junction", "biotic-zlo-distribution-channel-junction",
+  "biotic-zlo-distribution-aluminum-router", "biotic-zlo-distribution-channel-router",
+  "biotic-zlo-distribution-aluminum-bridge", "biotic-zlo-distribution-channel-bridge",
+  "biotic-zlo-liquid-liquid-junction", "biotic-zlo-liquid-fluid-junction",
+  "biotic-zlo-liquid-liquid-router", "biotic-zlo-liquid-fluid-router",
+  "biotic-zlo-liquid-liquid-bridge", "biotic-zlo-liquid-fluid-bridge"
+)
+
 
