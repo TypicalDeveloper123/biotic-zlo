1b0eb7fc31fbe0c75e876d3c3fd45d95223b288e
Bismutotantalite/Biz | Corpuscles <reymarkgaming123@gmail.com>
2026-04-29 07:53:45 +0800

    a

--- scripts/main.js
+++ scripts/main.js
@@ -11,15 +11,28 @@ SaveFileReader.fallback.putAll(
   "corpuscles-biotic-zlo-distribution-aluminum-junction", "corpuscles-biotic-zlo-distribution-channel-junction",
   "corpuscles-biotic-zlo-distribution-aluminum-router", "corpuscles-biotic-zlo-distribution-channel-router",
   "corpuscles-biotic-zlo-distribution-aluminum-bridge", "corpuscles-biotic-zlo-distribution-channel-bridge",
+
   "corpuscles-biotic-zlo-liquid-liquid-junction", "corpuscles-biotic-zlo-liquid-fluid-junction",
   "corpuscles-biotic-zlo-liquid-liquid-router", "corpuscles-biotic-zlo-liquid-fluid-router",
   "corpuscles-biotic-zlo-liquid-liquid-bridge", "corpuscles-biotic-zlo-liquid-fluid-bridge",
+
   "corpuscles-biotic-zlo-a1-liquid-blood", "corpuscles-biotic-zlo-liquid-liquidblood",
   "corpuscles-biotic-zlo-tile-BLOOD", "corpuscles-biotic-zlo-tile-COAGULATED-BLOOD",
   "corpuscles-biotic-zlo-wall-BLOOD", "corpuscles-biotic-zlo-wall-COAGULATED-BLOOD",
+
   "corpuscles-blood-boulder", "corpuscles-coagulated-blood-boulder",
   "corpuscles-biotic-zlo-wall-BONE", "corpuscles-biotic-zlo-wall-DENSE-BONE",
-  "corpuscles-biotic-zlo-wall-FLESH", "corpuscles-biotic-zlo-wall-DENSE-FLESH"
+  "corpuscles-biotic-zlo-wall-FLESH", "corpuscles-biotic-zlo-wall-DENSE-FLESH",
+
+  "corpuscles-biotic-zlo-wall-BLOODSTONE", "corpuscles-biotic-zlo-wall-SCORIA",
+  "corpuscles-biotic-zlo-tile-BLOODSTONE", "corpuscles-biotic-zlo-tile-SCORIA",
+  "corpuscles-bloodstone-boulder", "corpuscles-scoria-boulder",
+  "corpuscles-bloodstone-vent", "corpuscles-scoria-vent",
+
+  "corpuscles-biotic-zlo-wall-HEMATITE", "corpuscles-biotic-zlo-wall-LODESTONE",
+  "corpuscles-biotic-zlo-tile-HEMATITE", "corpuscles-biotic-zlo-tile-LODESTONE",
+  "corpuscles-hematite-boulder", "corpuscles-lodestone-boulder",
+  "corpuscles-hematite-vent", "corpuscles-lodestone-vent"
 )
 
 
